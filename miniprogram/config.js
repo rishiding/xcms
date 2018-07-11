e6203477f48c88e0acd3a121dc48e38298571f9e
rishi <qianfo_713@163.com>
2018-07-11 19:32:45 +0800

    修改

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,15 +1,15 @@
 /**
  * 小程序配置文件
  */
-var host = "localhost:9090"
+var host = "wx.scdelifu.com"
 
 var config = {
     // 下面的地址配合云端 Server 工作
     host,
-    server: `http://${host}`,
+    server: `https://${host}`,
     hospitalid:`1`,
     // API 接口
-    appApiUrl: `http://${host}/f/api`
+    appApiUrl: `https://${host}/f/api`
 }; 
 
 module.exports = config
