e7081ddf7d9f6fd27a6d62a4dceb73ca059152a2
rishi <qianfo_713@163.com>
2018-07-17 18:15:42 +0800

    名字修改

--- miniprogram/pages/home/pages/hospital/index.js
+++ miniprogram/pages/home/pages/hospital/index.js
@@ -13,7 +13,7 @@ Page({
   onLoad: function () {
     // 页面初始化 options为页面跳转所带来的参数
     wx.setNavigationBarTitle({
-      title: '德里福健康',
+      title: '众医康',
     })
     var that = this;
     wx.getSystemInfo({
